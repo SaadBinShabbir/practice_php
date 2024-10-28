583595ab4a65cfb8babd502902ba88c906db09c9
Saad Bin Shabbir <164950489+SaadBinShabbir@users.noreply.github.com>
2024-10-28 21:10:12 +0500

    Add files via upload

--- practice_ssion4.php
+++ practice_ssion4.php
@@ -40,4 +40,42 @@ $new_std = sort($std);
 
 echo "<hr>";
 
+//age
+
+$age = [20,21,35,12];
+sort($age); //sort
+echo "<br>";
+print_r($age);
+echo "<br>";
+
+rsort($age);
+print_r($age);
+echo "<br>";
+echo "<hr>";
+
+$names = ["name1"=>"Saad","name2"=>"Uzair","name3"=>"Ali","name4"=>"Asim"];
+print_r($names);
+echo "<br>";
+
+asort($names);
+print_r($names);
+
+echo "<br>";
+echo "<hr>";
+
+$sbs2b1 = [
+
+    ["saad",20,"saad@gmail.com"],
+    ["ali",30,"ali@gmail.com"],
+    ["asim",18,"Asim@gmail.com"],
+
+
+];
+
+print_r($sbs2b1);
+echo "<br>";
+echo "<br>";
+echo $sbs2b1[1][2];
+
+
 ?>
